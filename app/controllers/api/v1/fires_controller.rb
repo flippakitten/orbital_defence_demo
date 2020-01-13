fc968b597a41aff3b0565981f8920d28f010911b
James Gascoigne-Taylor <james@flippakitten.com>
2020-01-13 10:06:36 +0000

    remove wind direction from map load

--- app/controllers/api/v1/fires_controller.rb
+++ app/controllers/api/v1/fires_controller.rb
@@ -6,10 +6,6 @@ class Api::V1::FiresController < ApplicationController
 
   private
 
-  def populate_fires
-
-  end
-
   def fire_params
     params.permit(:latitude, :longitude, :distance, :sw_bound_point, :ne_bound_point, :bust_cache)
   end
