60436475c47ede4ebe90d9432a0732a583dd2035
James Gascoigne-Taylor <james@flippakitten.com>
2020-01-17 10:21:11 +0000

    rmeove weather stations from serializer

--- app/serializers/api/v1/fire_serializer.rb
+++ app/serializers/api/v1/fire_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::FireSerializer < ActiveModel::Serializer
-  attributes :id, :scan_type, :detected_at, :confidence, :latitude, :longitude, :weather_reading_id, :weather_station
+  attributes :id, :scan_type, :detected_at, :confidence, :latitude, :longitude, :weather_reading_id
 
   def id
     object.id.to_s
