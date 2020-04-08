b670fe71eacb05f19fc0bb262574a121289b3968
James Gascoigne-Taylor <james@flippakitten.com>
2020-04-08 07:56:06 +0100

    expire fire cache in 2 hours

--- app/workers/firms_import_worker.rb
+++ app/workers/firms_import_worker.rb
@@ -3,6 +3,6 @@ class FirmsImportWorker
 
   def perform
     Nasa::FirmsImport.all
-    Rails.cache.write('fires-in_last_24_hours', Fire.in_last_24_hours)
+    Rails.cache.write('fires-in_last_24_hours', Fire.in_last_24_hours, expires_in: 2.hours)
   end
 end
