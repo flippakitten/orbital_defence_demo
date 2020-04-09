e9d1ee805b8cbbeab4b22e6a85c169e97a82c3ea
James Gascoigne-Taylor <james@flippakitten.com>
2020-04-09 08:37:24 +0100

    sidekiq-unique-jobs

--- app/workers/firms_import_worker.rb
+++ app/workers/firms_import_worker.rb
@@ -1,6 +1,8 @@
 class FirmsImportWorker
   include Sidekiq::Worker
 
+  sidekiq_options lock: :until_executed, unique_across_queues: true, on_conflict: :reject
+
   def perform
     Nasa::FirmsImport.all
     Rails.cache.write('fires-in_last_24_hours', Fire.in_last_24_hours, expires_in: 2.hours)
--- app/workers/weather_import_worker.rb
+++ app/workers/weather_import_worker.rb
@@ -1,6 +1,8 @@
 class WeatherImportWorker
   include Sidekiq::Worker
 
+  sidekiq_options lock: :until_executed, unique_across_queues: true, on_conflict: :reject
+
   def perform
     Fire.in_last_24_hours.each do |fire|
       FireWeatherData.new(fire).find_or_create_weather
