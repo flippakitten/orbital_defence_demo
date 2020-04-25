495aa2d463347e8c426ceae4468af1bbd6c66608
James Gascoigne-Taylor <james@flippakitten.com>
2020-04-25 07:43:46 +0100

    update weather import worker cache time

--- app/workers/weather_import_worker.rb
+++ app/workers/weather_import_worker.rb
@@ -4,7 +4,7 @@ class WeatherImportWorker
   def perform
     return if Rails.cache.read('WeatherImportWorker')
 
-    Rails.cache.write('WeatherImportWorker', true, expires_in: 1.hour)
+    Rails.cache.write('WeatherImportWorker', true, expires_in: 2.hour)
 
     Fire.in_last_24_hours.each do |fire|
       FireWeatherData.new(fire).find_or_create_weather
